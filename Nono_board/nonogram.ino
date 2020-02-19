6ffa518789a53e7b823823b400399b8a4887b6d3
LonLim <46553551+LonLim@users.noreply.github.com>
2020-02-19 23:17:20 +0800

    Update nonogram.ino

--- Nono_board/nonogram.ino
+++ Nono_board/nonogram.ino
@@ -1,108 +1,132 @@
-int x = 0;
+int playerPosition = 0;
+
 int location[64];
+
+//Pins & Variables for analog joystick
+#define analog_x A0
+#define analog_y A1
+#define analog_button 52
+int x_value = 0;
+int y_value = 0;
+int swtich = 0;
 
 void setup() {
   // open the serial port:
   Serial.begin(9600);
-  updateLocation(x);
-
+  digitalWrite(analog_button, HIGH);
+  pinMode(analog_x, INPUT);
+  pinMode(analog_y, INPUT);
+  updateLocation(playerPosition);
 }
-void loop() {
-  // check for incoming serial data:
-  char inChar = 0;
-  if (Serial.available() > 0) {
-    // read incoming serial data:
-   inChar = Serial.read();
-    // Type the next ASCII value from what you received:
-  }
-  if (inChar =='w')
-  {
-    x = x-1;
-    x=checkVerticalBountary(x, inChar);
-    updateLocation(x);
-  }
 
-  else if (inChar =='s')
+void loop() {
+  // check for incoming data from joystick
+  x_value = analogRead(analog_x);
+  y_value = analogRead(analog_y);
+  swtich = digitalRead(analog_button);
+  
+  if (y_value < 400)
   {
-    x = x+1;
-    x=checkVerticalBountary(x, inChar);
-    updateLocation(x);
+    playerPosition = playerPosition - 1;
+    playerPosition = checkTopBountary(playerPosition);
+    updateLocation(playerPosition);
   }
-  else if (inChar =='a')
+  else if (y_value > 600)
   {
-    x = x-8;
-    x=checkHorizontalBountary(x);
-    updateLocation(x);
+    playerPosition = playerPosition + 1;
+    playerPosition = checkBottomBountary(playerPosition);
+    updateLocation(playerPosition);
   }
-  else if (inChar == 'd')
+  else if (x_value < 400)
   {
-    x = x+8;
-    x=checkHorizontalBountary(x);
-    updateLocation(x);
+    playerPosition = playerPosition - 8;
+    playerPosition = checkSideBountary(playerPosition);
+    updateLocation(playerPosition);
   }
-  else if (inChar =='f')
+  else if (x_value > 600)
   {
-    if(location[x]==1)
+    playerPosition = playerPosition + 8;
+    playerPosition = checkSideBountary(playerPosition);
+    updateLocation(playerPosition);
+  }
+  else if (swtich == LOW)
+  {
+    if(location[playerPosition] == 1)
     {
-      location[x]=2;
+      location[playerPosition] = 2;
     }
     else
     {
-      location[x]=0;
+      location[playerPosition] = 0;
     }
-    updateLocation(x);
+    updateLocation(playerPosition);
   }
 }
-void updateLocation (int x)
-{
+
+void updateLocation (int playerPosition)  { // Update the location of the pixel
   //memset(location,0,sizeof(location));
   //easiest way to reset all array to zero
-  initLocation();
-  if (location[x] != 2) {location[x] = 1;}
-  for (int j = 0; j < 8; j++){
-  for (int i = 0; i < 8; i++) 
+  initLocation(playerPosition);
+  if (location[playerPosition] != 2) 
   {
-    Serial.print(location[j+8*i]);
-    Serial.print("  ");
+    location[playerPosition] = 1;
   }
-  Serial.println("");
-  }
-Serial.println("----------------------------------------------");
-  
-}
-int checkHorizontalBountary(int x)
-{
-  if (x<0)
+  for (int row = 0; row < 8; row++)
   {
-    x = x+64;
-  }
-  else if (x>63)
-  {
-    x = x-64;
-  }
-  return x;
-}
-
-int checkVerticalBountary(int x, char joystick)
-{
-   if ((x+1)%8==0 && joystick=='w')
-  {
-    x = x+8;
-  }
-  else if (x%8 ==0 && joystick=='s')
-  {
-    x = x-8;
-  }
-  return x;
-}
-
-void initLocation()
-{
-   for (int j = 0; j < 64; j++)
-   {
-      if(location[j]!=2)
+    for (int col = 0; col < 8; col++) 
       {
-        location[j]=0;
+        Serial.print(location[j+8*i]);
+        Serial.print("  ");
+      }
+    Serial.println("");
+  }
+  Serial.println("----------------------------------------------");
+  Serial.println(playerPosition);
+  delay(500);
+}
+
+int checkSideBountary(int playerPosition) { // check if position has moved out of side boundary
+  if (playerPosition < 0)
+  {
+    playerPosition = playerPosition + 64;
+  }
+  else if (playerPosition > 63)
+  {
+    playerPosition = playerPosition - 64;
+  }
+  return playerPosition;
+}
+
+int checkBottomBountary(int playerPosition) { // check if position has moved out of bottom boundary
+  if (playerPosition % 8 == 0)
+  {
+    playerPosition = playerPosition - 8;
+  }
+  return playerPosition;
+}
+
+int checkTopBountary(int playerPosition) { // check if position has moved out of top boundary
+  if ((playerPosition + 1) % 8 == 0)
+  {
+    playerPosition = playerPosition + 8;
+  }
+  return playerPosition;
+}
+
+void initLocation(int playerPosition) { // init all pixel and change to its relevant number accordingly
+   for (int position = 0; position < 64; position++)
+   {
+      if(location[position] != 2)
+      {
+        location[position] = 0;
+      }
+      if(location[position] == 2 || location[position] == 3)
+      {
+        location[position] = 2;
+      }
+      if(location[position] == 2 && position == playerPosition)
+      {
+        location[position = 3;
       }
    }
 }
