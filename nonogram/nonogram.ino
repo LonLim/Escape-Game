b01cf0f485e2ce9722364680306a550c4c4cdba2
LonLim <46553551+LonLim@users.noreply.github.com>
2020-02-26 23:07:50 +0800

    Update nonogram.ino

--- nonogram/nonogram.ino
+++ nonogram/nonogram.ino
@@ -25,10 +25,41 @@ int swtich = 0;
 //Variable and array for Clue(Zhi Sheng to insert the array in)
 int clue_row = 0;
 int clue_array[3][28] = {
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {11, 4, 2, 1, 111, 4, 112, 3, 3311, 0, 2214, 0, 1, 4, 13, 4, 52, 4, 1, 4, 51, 4, 13, 4, 4, 4, 1, 4},
+  {1, 0, 1, 0, 111, 4, 22, 2, 3212, 0, 1415, 0, 1, 4, 2, 4, 5, 4, 13, 4, 211, 4, 311, 4, 31, 4, 1, 4},
+  {2, 4, 1, 0, 11, 4, 32, 2, 7131, 0, 1223, 0, 1, 4, 42, 4, 122, 4, 111, 4, 132, 4, 112, 4, 14, 4, 1, 4},
 };
+
+//Define for all Display
+#include <TM1637Display.h> 
+#define CLK 14
+#define DIO1 1
+#define DIO2 2
+#define DIO3 3
+#define DIO4 4
+#define DIO5 5
+#define DIO6 6
+#define DIO7 7
+#define DIO8 8
+#define DIO9 9
+#define DIO10 10
+#define DIO11 11
+#define DIO12 12
+
+TM1637Display display1(9, 1); 
+TM1637Display display2(9, 2); 
+TM1637Display display3(9, 3);
+TM1637Display display4(9, 4);
+TM1637Display display5(9, 5); 
+TM1637Display display6(9, 6); 
+TM1637Display display7(9, 7);
+TM1637Display display8(9, 8); 
+TM1637Display display9(9, 9); 
+TM1637Display display10(9, 10); 
+TM1637Display display11(9, 11);
+TM1637Display display12(9, 12);
+
+uint8_t data[] = { 0x0, 0x0, 0x0, 0x0 };  // all segments clear
 
 void setup() {
   // open the serial port:
@@ -38,9 +69,38 @@ void setup() {
   pinMode(analog_y, INPUT);
   updateLocation(playerPosition);
   pinMode(reset, INPUT);
+
+  //Set Brightness for all display
+  display1.setBrightness(0x0f);
+  display2.setBrightness(0x0f);
+  display3.setBrightness(0x0f);
+  display4.setBrightness(0x0f);
+  display5.setBrightness(0x0f);
+  display6.setBrightness(0x0f);
+  display7.setBrightness(0x0f);
+  display8.setBrightness(0x0f);
+  display9.setBrightness(0x0f);
+  display10.setBrightness(0x0f);
+  display11.setBrightness(0x0f);  
+  display12.setBrightness(0x0f); 
+ 
+  //Fill all display with empty data
+  display1.setSegments(data);
+  display2.setSegments(data);  
+  display3.setSegments(data);
+  display4.setSegments(data);
+  display5.setSegments(data);
+  display6.setSegments(data);
+  display7.setSegments(data);
+  display8.setSegments(data);
+  display9.setSegments(data);
+  display10.setSegments(data);  
+  display11.setSegments(data);
+  display12.setSegments(data);
 }
 
 void loop() {
+  checkReset();
   displayClue();
   
   // check for incoming data from joystick
@@ -154,6 +214,7 @@ void initLocation(int playerPosition) { // init all pixel and change to its rele
       }
    }
 }
+
 void setLEDColor()  {    
    for(int position = 0; position < 64; position++){ //update location to the pixel
       switch (location[position]){
@@ -185,6 +246,40 @@ void checkReset() {
   }
 }
 
-void displayClue() { //(Zhi Sheng to insert the code of all 14 7 segments)
+void displayClue() {
+  display1.setSegments(data);// fill(clear) display with data[] clue_array
+  display1.showNumberDec(clue_array[clue_row][0], 0, clue_array[clue_row][1]);// display 1
+
+  display2.setSegments(data);// fill(clear) display with data[] clue_array  
+  display2.showNumberDec(clue_array[clue_row][2], 0, clue_array[clue_row][3]);// display 2
+
+  display3.setSegments(data);// fill(clear) display with data[] clue_array
+  display3.showNumberDec(clue_array[clue_row][4], 0, clue_array[clue_row][5]);// display 3
+
+  display4.setSegments(data);// fill(clear) display with data[] clue_array
+  display4.showNumberDec(clue_array[clue_row][6], 0, clue_array[clue_row][7]);// display 4
   
+  display5.setSegments(data);// fill(clear) display with data[] clue_array
+  display5.showNumberDec(clue_array[clue_row][8], 0, clue_array[clue_row][9]);// display 5
+
+  display6.setSegments(data);// fill(clear) display with data[] clue_array  
+  display6.showNumberDec(clue_array[clue_row][10], 0, clue_array[clue_row][11]);// display 6
+
+  display7.setSegments(data);// fill(clear) display with data[] clue_array
+  display7.showNumberDec(clue_array[clue_row][12], 0, clue_array[clue_row][13]);// display 7
+
+  display8.setSegments(data);// fill(clear) display with data[] clue_array
+  display8.showNumberDec(clue_array[clue_row][14], 0, clue_array[clue_row][15]);// display 8
+  
+  display9.setSegments(data);// fill(clear) display with data[] clue_array
+  display9.showNumberDec(clue_array[clue_row][16], 0, clue_array[clue_row][17]);// display 9
+
+  display10.setSegments(data);// fill(clear) display with data[] clue_array  
+  display10.showNumberDec(clue_array[clue_row][18], 0, clue_array[clue_row][19]);// display 10
+
+  display11.setSegments(data);// fill(clear) display with data[] clue_array
+  display11.showNumberDec(clue_array[clue_row][20], 0, clue_array[clue_row][21]);// display 11
+
+  display12.setSegments(data);// fill(clear) display with data[] clue_array
+  display12.showNumberDec(clue_array[clue_row][22], 0, clue_array[clue_row][23]);// display 12
 }
