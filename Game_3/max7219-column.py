a92bf1c64702575f816b8de96d6c01bff1e793be
jamekwok <46553589+jamekwok@users.noreply.github.com>
2020-03-13 23:35:47 +0800

    added

--- Game_3/max7219-column.py
+++ Game_3/max7219-column.py
@@ -7,8 +7,22 @@ serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial)
 
 
-for y in range(0,8):
-    for x in range(0,8):
-        with canvas(device) as draw:
-            draw.point((x, y), fill="white")
-        time.sleep(0.5)
+while True:
+    
+ with canvas(device) as draw:
+    draw.line((0, 0, 0, device.height), fill="red")
+    time.sleep(0.5)
+    draw.line((1, 0, 1, device.height), fill="orange")
+    time.sleep(0.5)
+    draw.line((2, 0, 2, device.height), fill="yellow")
+    time.sleep(0.5)
+    draw.line((3, 0, 3, device.height), fill="green")
+    time.sleep(0.5)
+    draw.line((4, 0, 4, device.height), fill="blue")
+    time.sleep(0.5)
+    draw.line((5, 0, 5, device.height), fill="indigo")
+    time.sleep(0.5)
+    draw.line((6, 0, 6, device.height), fill="violet")
+    time.sleep(0.5)
+    draw.line((7, 0, 7, device.height), fill="white")    
+    time.sleep(0.5)
