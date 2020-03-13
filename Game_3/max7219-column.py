429e3c90bd7c7a8a6f4a41749e1c19514be40c00
jamekwok <46553589+jamekwok@users.noreply.github.com>
2020-03-13 23:21:00 +0800

    added

--- Game_3/max7219-column.py
+++ Game_3/max7219-column.py
@@ -1,5 +1,11 @@
 from luma.core.legacy import text
 from luma.core.legacy.font import proportional, CP437_FONT, LCD_FONT
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.led_matrix.device import max7219
+
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial)
 
 with canvas(device) as draw:
     text(draw, (0, 0), "1", fill="white", font=proportional(CP437_FONT))
