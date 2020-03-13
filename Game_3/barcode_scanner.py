eb41109f32056bdd307ecc648895b6bf6b0095ce
jamekwok <46553589+jamekwok@users.noreply.github.com>
2020-03-13 17:39:26 +0800

    changed video frame grayscale

--- Game_3/barcode_scanner.py
+++ Game_3/barcode_scanner.py
@@ -43,8 +43,8 @@ while True:
 	for barcode in barcodes:
 		# extract the bounding box location of the barcode and draw
 		# the bounding box surrounding the barcode on the image
-		(x, y, w, h) = barcode.rect
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+		#(x, y, w, h) = barcode.rect
+		#cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
 		# the barcode data is a bytes object so if we want to draw it
 		# on our output image we need to convert it to a string first
@@ -53,7 +53,7 @@ while True:
 
 		# draw the barcode data and barcode type on the image
 		text = "{} ({})".format(barcodeData, barcodeType)
-		cv2.putText(frame, text, (x, y - 10),
+		#cv2.putText(frame, text, (x, y - 10),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
 		# if the barcode text is currently not in our CSV file, write
@@ -67,7 +67,7 @@ while True:
 	# show the output frame
 	cv2.imshow("Barcode Scanner", frame)
 	key = cv2.waitKey(1) & 0xFF
- 
+	print(text)
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
