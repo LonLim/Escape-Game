8e121f5fde764a87eb89b027cb4293e725e8f841
jamekwok <46553589+jamekwok@users.noreply.github.com>
2020-03-13 22:44:42 +0800

    adafruit

--- Game_3/max7219-column.py
+++ Game_3/max7219-column.py
@@ -1,38 +1,30 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright (c) 2017 Richard Hull and contributors
-# See LICENSE.rst for details.
-
-import re
+from adafruit_max7219 import matrices
+from board import TX, RX, A2
+import busio
+import digitalio
 import time
-import argparse
 
-from luma.led_matrix.device import max7219
-from luma.core.interface.serial import spi, noop
-from luma.core.render import canvas
-from luma.core.legacy import text, show_message
+clk = RX
+din = TX
+cs = digitalio.DigitalInOut(A2)
 
-def demo(n, block_orientation, rotate):
-    # create matrix device
-    serial = spi(port=0, device=1, gpio=noop())
-    device = max7219(serial, cascaded=n or 1,  block_orientation=block_orientation, rotate=rotate or 0)
-    print("Created device")
-    for x in range(256):
-        with canvas(device) as draw:
-            text(draw, (0, 0), chr(x), fill="white")
-            time.sleep(0.1)
+spi = busio.SPI(clk, MOSI=din)
+display = matrices.Matrix8x8(spi, cs)
+while True:
+    display.brightness(3)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='matrix_demo arguments',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    display.fill(1)
+    display.pixel(3, 3)
+    display.pixel(3, 4)
+    display.pixel(4, 3)
+    display.pixel(4, 4)
+    display.show()
+    time.sleep(3.0)
 
-    parser.add_argument('--cascaded', '-n', type=int, default=1, help='Number of cascaded MAX7219 LED matrices')
-    parser.add_argument('--block-orientation', type=int, default=0, choices=[0, 90, -90], help='Corrects block orientation when wired vertically')
-    parser.add_argument('--rotate', type=int, default=0, choices=[0, 1, 2, 3], help='Rotate display 0=0°, 1=90°, 2=180°, 3=270°')
-
-    args = parser.parse_args()
-
-    try:
-        demo(args.cascaded, args.block_orientation, args.rotate)
-    except KeyboardInterrupt:
-        pass
+    display.clear_all()
+    s = 'Hello, World!'
+    for c in range(len(s)*8):
+        display.fill(0)
+        display.text(s,-c,0)
+        display.show()
+        time.sleep(0.25)
