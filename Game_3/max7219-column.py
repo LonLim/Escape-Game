9fa5229b11083430124f924047ba3dd91ca3192d
jamekwok <46553589+jamekwok@users.noreply.github.com>
2020-03-13 23:31:14 +0800

    Update max7219-column.py

--- Game_3/max7219-column.py
+++ Game_3/max7219-column.py
@@ -7,8 +7,8 @@ serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial)
 
 
-for y in range(device.height):
-    for x in range(device.width):
+for y in range(0,8):
+    for x in range(0,8):
         with canvas(device) as draw:
             draw.point((x, y), fill="white")
         time.sleep(0.5)
