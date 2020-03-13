4bea3655f1b0d8393d96c1e694735b1b5026dc73
jamekwok <46553589+jamekwok@users.noreply.github.com>
2020-03-13 23:30:15 +0800

    added

--- Game_3/max7219-column.py
+++ Game_3/max7219-column.py
@@ -1,11 +1,11 @@
-import time
-
-from luma.led_matrix.device import unicornhathd
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
+from luma.led_matrix.device import max7219
+import time
 
 serial = spi(port=0, device=0, gpio=noop())
-device = unicornhathd(serial)
+device = max7219(serial)
+
 
 for y in range(device.height):
     for x in range(device.width):
