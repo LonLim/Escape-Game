a3406b6b7bb65d7e46e66117a025870e97e797ce
jamekwok <46553589+jamekwok@users.noreply.github.com>
2020-03-13 22:18:22 +0800

    import library

--- Game_3/max7219-column.py
+++ Game_3/max7219-column.py
@@ -10,6 +10,7 @@ import argparse
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
+from luma.core.legacy import text, show_message
 
 def demo(n, block_orientation, rotate):
     # create matrix device
