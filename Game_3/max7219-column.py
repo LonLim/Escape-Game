11d98f245d842873b257b784212cc43f20e85e7f
jamekwok <46553589+jamekwok@users.noreply.github.com>
2020-03-14 03:13:42 +0800

    Update max7219-column.py

--- Game_3/max7219-column.py
+++ Game_3/max7219-column.py
@@ -12,7 +12,7 @@ device = max7219(serial,rotate=2)
 str = ""
 while True:
     c = sys.stdin.read(1) # reads one byte at a time, similar to getchar()
-    bytes = bin(int(c))
+    bytes = bin(int(c[0:-2]))
     
     for index, item in enumerate(bytes):
         with canvas(device) as draw:
