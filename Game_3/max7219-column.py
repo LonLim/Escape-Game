be01802d63d0dc5907866a079b1a954e384a1122
jamekwok <46553589+jamekwok@users.noreply.github.com>
2020-03-14 03:54:24 +0800

    Update max7219-column.py

--- Game_3/max7219-column.py
+++ Game_3/max7219-column.py
@@ -13,7 +13,8 @@ len = 0
 while True:
     c = sys.stdin.read(1) # reads one byte at a time, similar to getchar()
     if(c.isdigit()):
-        bytes = "{0:b}".format(int(c))
+        bytes = "{0:04b}".format(int(c))
+        if len(bytes)
         print(bytes)
     with canvas(device) as draw:
         for index,item in enumerate(bytes):
